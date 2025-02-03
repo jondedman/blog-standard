a818b1035f42cb95aa40c1bc696f7c143d0d3130
Jon Dedman <jonathandedman@googlemail.com>
2025-02-03 19:45:27 +0000

    add loading icon and further styling

--- pages/post/new.js
+++ pages/post/new.js
@@ -3,6 +3,8 @@ import { AppLayout } from "../../components/AppLayout/AppLayout";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { getAppProps } from "../../utils/getAppProps";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBrain } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function NewPost(props) {
@@ -11,10 +13,12 @@ export default function NewPost(props) {
   const router = useRouter();
   const [topic, setTopic] = useState("");
   const [keywords, setKeywords] = useState("");
+  const [generating , setGenerating] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Generating post");
+    setGenerating(true);
+    try {
     const response = await fetch('/api/generatePost', {
       method: 'POST',
       headers: {
@@ -27,11 +31,23 @@ export default function NewPost(props) {
     if(json?.postId) {
       router.push(`/post/${json.postId}`);
     }
+    } catch (error) {
+      setGenerating(false);
+    }
+
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="h-full overflow-hidden">
+      {!!generating && (
+      <div className="text-green-500 flex h-full animate-pulse w-full flex flex-col items-center justify-center">
+        <FontAwesomeIcon icon={faBrain} className="text-8xl" />
+        <h6>Generating...</h6>
+      </div>
+      )}
+      {!generating && (
+      <div className="w-full h-full flex flex-col overflow-auto">
+      <form onSubmit={handleSubmit} className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200 ">
         <div>
           <label>
             <strong>Generate a blog post on the topic of</strong>
@@ -43,12 +59,17 @@ export default function NewPost(props) {
             <strong>Targetting the following keywords:</strong>
         </label>
         <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={(e) => setKeywords(e.target.value)} />
+          <small className="block mb-2">
+            Separate keywords with commas
+          </small>
         </div>
         <button type="submit" className="btn">
       Generate
       </button>
     </form>
+      </div>
 
+      )}
     </div>
   );
 }
